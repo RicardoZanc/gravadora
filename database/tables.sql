f1b549ab6f17cacececdd0045930bbdc97b69013
Ricardo <ricardozancanc@gmail.com>
2023-11-03 20:08:35 -0300

    database created

--- database/tables.sql
+++ database/tables.sql
@@ -2,11 +2,10 @@ create table artists (
 
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    banda_artist CHAR NOT NULL,
+    band_artist CHAR NOT NULL CHECK (band_artist='b' or band_artist='a'),
     birth_formation DATE NOT NULL,
-    created_in DATE NOT NULL DEFAULT current_timestamp(),
-    edited_in DATE NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT CHK_Artist CHECK (banda_artist='b' or band_artist='a')
+    created_in TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    edited_in TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP
 
 );
 
@@ -14,8 +13,8 @@ create table genres (
 
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    created_in DATE NOT NULL DEFAULT current_timestamp(),
-    edited_in DATE NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP
+    created_in TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    edited_in TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP
 
 );
 
@@ -23,7 +22,22 @@ create table albuns (
 
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    description VARCHAR,
-    genre int REFERENCES  
+    description VARCHAR(255),
+    release_date DATE NOT NULL,
+    genre int NOT NULL REFERENCES genres(id) ,
+    artist int NOT NULL REFERENCES artists(id),
+    created_in TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    edited_in TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP   
 
 );
+
+create table musics (
+
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    album int REFERENCES albuns(id),
+    created_in TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    edited_in TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP   
+
+);
+
